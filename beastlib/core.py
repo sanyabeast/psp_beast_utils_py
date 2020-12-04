400c1eb5a207422afecbe7a48c63d977c253bb5b
sanyabeast <a.gvrnsk@gmail.com>
2020-12-04 18:05:24 +0200

    core: PadButtonsObserver

--- beastlib/core.py
+++ beastlib/core.py
@@ -13,6 +13,7 @@ import stackless
 import sys
 from beastlib.types import *
 
+DEFAULT_BUTTON_THROTTLE = 0.025
 screen = psp2d.Screen()
 screen.clear(psp2d.Color(0,0,0,255))
 SCREEN_W = 480
@@ -59,3 +60,64 @@ class Tickable(CoreObject):
     def on_tick(self):
         pass
 
+class PadButtonsObserver(CoreObject):
+    TAG = "padbuttonsobserver"
+    pad_buttons_observe_enabled = False
+    def __init__(self, props={}):
+        CoreObject.__init__(self, props)
+        now = time()
+        
+        self.pad_buttons_observer_state = {
+            "button_throttle": {
+                "cross":DEFAULT_BUTTON_THROTTLE,
+                "triangle":DEFAULT_BUTTON_THROTTLE,
+                "square":DEFAULT_BUTTON_THROTTLE,
+                "circle":DEFAULT_BUTTON_THROTTLE,
+                "up":DEFAULT_BUTTON_THROTTLE,
+                "down":DEFAULT_BUTTON_THROTTLE,
+                "left": DEFAULT_BUTTON_THROTTLE,
+                "right":DEFAULT_BUTTON_THROTTLE
+            },
+            "prev_time": {
+                "cross":now,
+                "triangle":now,
+                "square":now,
+                "circle":now,
+                "up":now,
+                "down":now,
+                "left": now,
+                "right":now
+            }
+        }
+
+        self.pad_buttons_observe_enabled = self.get(props, "is_pawn", False)
+    def on_tick(self):
+        if self.pad_buttons_observe_enabled:
+            pad = GLOBAL["engine"].Controller()
+            if   pad.cross:     self.is_button_throttled("cross") and self.on_pad_cross(pad)
+            elif pad.triangle:  self.is_button_throttled("triangle") and self.on_pad_triangle(pad)
+            elif pad.circle:    self.is_button_throttled("circle") and self.on_pad_circle(pad)
+            elif pad.square:    self.is_button_throttled("square") and self.on_pad_square(pad)
+            elif pad.down:      self.is_button_throttled("down") and self.on_pad_down(pad)
+            elif pad.up:        self.is_button_throttled("up") and self.on_pad_up(pad)
+            elif pad.left:      self.is_button_throttled("left") and self.on_pad_left(pad)
+            elif pad.right:     self.is_button_throttled("right") and self.on_pad_right(pad)
+    def on_pad_cross(self, pad): pass
+    def on_pad_triangle(self, pad): pass
+    def on_pad_circle(self, pad): pass
+    def on_pad_square(self, pad): pass
+    def on_pad_down(self, pad): pass
+    def on_pad_up(self, pad): pass
+    def on_pad_left(self, pad): pass
+    def on_pad_right(self, pad): pass
+
+    def set_button_throttling(self, name, delay=DEFAULT_BUTTON_THROTTLE):
+        self.pad_buttons_observer_state["button_throttle"][name] = delay
+    def is_button_throttled(self, name):
+        delay = self.pad_buttons_observer_state["button_throttle"][name]
+        now = time()
+        if now - self.pad_buttons_observer_state["prev_time"][name]<delay:
+            return False
+        else:
+            self.pad_buttons_observer_state["prev_time"][name] = now
+            return True
--- beastlib/framework.py
+++ beastlib/framework.py
@@ -11,10 +11,10 @@ from time import time, localtime
 import datetime
 import stackless
 import sys
-from beastlib.core import CoreObject, Tickable, GLOBAL
+from beastlib.core import CoreObject, Tickable, GLOBAL, PadButtonsObserver, DEFAULT_BUTTON_THROTTLE
 from beastlib.types import *
 
-DEFAULT_BUTTON_THROTTLE = 0.025
+
 def_font = psp2d.Font('assets/font.png')    
 
 class Renderer(Tickable):
@@ -67,66 +67,36 @@ class Renderable(Tickable):
     def draw(self, screen):
         pass
 
-class Actor(Renderable):
+class Actor(Renderable, PadButtonsObserver):
     TAG = "actor"
     is_pawn = False
     pad_state = {}
     def __init__(self, props):
         Renderable.__init__(self, props)
-        now = time()
-        self.pad_state = {
-            "button_throttle": {
-                "cross":DEFAULT_BUTTON_THROTTLE,
-                "triangle":DEFAULT_BUTTON_THROTTLE,
-                "square":DEFAULT_BUTTON_THROTTLE,
-                "circle":DEFAULT_BUTTON_THROTTLE,
-                "up":DEFAULT_BUTTON_THROTTLE,
-                "down":DEFAULT_BUTTON_THROTTLE,
-                "left": DEFAULT_BUTTON_THROTTLE,
-                "right":DEFAULT_BUTTON_THROTTLE
-            },
-            "prev_time": {
-                "cross":now,
-                "triangle":now,
-                "square":now,
-                "circle":now,
-                "up":now,
-                "down":now,
-                "left": now,
-                "right":now
-            }
-        }
-        self.is_pawn = self.get(props, "is_pawn", False)
+        PadButtonsObserver.__init__(self, props)
     def on_tick(self):
-        if self.is_pawn:
-            pad = GLOBAL["engine"].Controller()
-            if   pad.cross:     self.is_button_throttled("cross") and self.on_pad_cross(pad)
-            elif pad.triangle:  self.is_button_throttled("triangle") and self.on_pad_triangle(pad)
-            elif pad.circle:  self.is_button_throttled("circle") and self.on_pad_circle(pad)
-            elif pad.square:  self.is_button_throttled("square") and self.on_pad_square(pad)
-            elif pad.down:      self.is_button_throttled("down") and self.on_pad_down(pad)
-            elif pad.up:        self.is_button_throttled("up") and self.on_pad_up(pad)
-            elif pad.left:      self.is_button_throttled("left") and self.on_pad_left(pad)
-            elif pad.right:     self.is_button_throttled("right") and self.on_pad_right(pad)
-    def on_pad_cross(self, pad): pass
-    def on_pad_triangle(self, pad): pass
-    def on_pad_circle(self, pad): pass
-    def on_pad_square(self, pad): pass
-    def on_pad_down(self, pad): pass
-    def on_pad_up(self, pad): pass
-    def on_pad_left(self, pad): pass
-    def on_pad_right(self, pad): pass
+        Renderable.on_tick(self)
+        PadButtonsObserver.on_tick(self)
+    #     if self.is_pawn:
+    #         pad = GLOBAL["engine"].Controller()
+    #         if   pad.cross:     self.is_button_throttled("cross") and self.on_pad_cross(pad)
+    #         elif pad.triangle:  self.is_button_throttled("triangle") and self.on_pad_triangle(pad)
+    #         elif pad.circle:    self.is_button_throttled("circle") and self.on_pad_circle(pad)
+    #         elif pad.square:    self.is_button_throttled("square") and self.on_pad_square(pad)
+    #         elif pad.down:      self.is_button_throttled("down") and self.on_pad_down(pad)
+    #         elif pad.up:        self.is_button_throttled("up") and self.on_pad_up(pad)
+    #         elif pad.left:      self.is_button_throttled("left") and self.on_pad_left(pad)
+    #         elif pad.right:     self.is_button_throttled("right") and self.on_pad_right(pad)
+    # def on_pad_cross(self, pad): pass
+    # def on_pad_triangle(self, pad): pass
+    # def on_pad_circle(self, pad): pass
+    # def on_pad_square(self, pad): pass
+    # def on_pad_down(self, pad): pass
+    # def on_pad_up(self, pad): pass
+    # def on_pad_left(self, pad): pass
+    # def on_pad_right(self, pad): pass
 
-    def set_button_throttling(self, name, delay=DEFAULT_BUTTON_THROTTLE):
-        self.pad_state["button_throttle"][name] = delay
-    def is_button_throttled(self, name):
-        delay = self.pad_state["button_throttle"][name]
-        now = time()
-        if now - self.pad_state["prev_time"][name]<delay:
-            return False
-        else:
-            self.pad_state["prev_time"][name] = now
-            return True
+    
 
 class DebugLog(Renderable):
     TAG = "debuglog"
@@ -143,4 +113,5 @@ class DebugLog(Renderable):
         self.lines.append(t)
         lines_len = len(self.lines)
         self.lines = self.lines[max(0, lines_len-self.max_log):lines_len]
+
 
